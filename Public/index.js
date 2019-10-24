808189b13ee59d83d822a3a4b8696c1f2a47ef72
puccinic <puccinic@uninorte.edu.co>
2019-10-24 01:05:17 -0500

    Se verifica el estado actual por tablero

--- Public/index.js
+++ Public/index.js
@@ -1,4 +1,26 @@
-$('.round-button').on('click',function(){
+$('.round-button').on('click',function() {
     $(this).toggleClass('red');
     $(this).toggleClass('green');
+});
+
+
+$(".btn.btn-info.btn-lg").on('click',function() {
+    data = {
+        Tablero: $(this).attr('id'),
+    }
+    $.get('/refresh', data, function(data){
+        const keys = Object.keys(data);
+        const values = Object.values(data);
+        for (let i = 1; i < keys.length-1; i++){
+            if (keys[i] == 'emergencia') continue;
+            switch (values[i]){
+                case 1:
+                    $('#'+keys[i]).addClass('green').removeClass('red');
+                    break;
+                default:
+                    $('#'+keys[i]).addClass('red').removeClass('green');
+                    break;
+            } 
+        } 
+    });
 });
