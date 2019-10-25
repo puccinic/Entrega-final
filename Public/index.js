f8a94c1023c4620c941e9d7526db7d14543bf220
Sebastian <sdmejia@uninorte.edu.co>
2019-10-25 11:33:34 -0500

    luz emergencia

--- Public/index.js
+++ Public/index.js
@@ -3,6 +3,40 @@ $('.round-button').on('click',function() {
     $(this).toggleClass('green');
 });
 
+$('#emergencia').click(function() {
+  $('#indicador').toggleClass('invisible');
+  $('#indicador').toggleClass('visible');
+  $('#gen1').removeClass('green');
+  $('#gen2').removeClass('green');
+  $('#gen3').removeClass('green');
+  $('#carga').removeClass('green');
+  $('#linea').removeClass('green');
+  $('#capacitor').removeClass('green');
+  $('#emergencia').toggleClass('blue');
+  if ($("#emergencia").text("Emergencia")) {
+      $("#emergencia").text("Reactivar");
+   }
+   else {
+      $("#emergencia").text("Emergencia");
+   }
+
+  if ($("#gen1").attr("disabled") || $("#gen2").attr("disabled") || $("#gen3").attr("disabled")
+      || $("#carga").attr("disabled") || $("#linea").attr("disabled") || $("#capacitor").attr("disabled")) {
+        $("#gen1").removeAttr("disabled");
+        $("#gen2").removeAttr("disabled");
+        $("#gen3").removeAttr("disabled");
+        $("#carga").removeAttr("disabled");
+        $("#linea").removeAttr("disabled");
+        $("#capacitor").removeAttr("disabled");
+    } else {
+        $("#gen1").attr("disabled", "disabled");
+        $("#gen2").attr("disabled", "disabled");
+        $("#gen3").attr("disabled", "disabled");
+        $("#carga").attr("disabled", "disabled");
+        $("#linea").attr("disabled", "disabled");
+        $("#capacitor").attr("disabled", "disabled");
+    }
+});
 
 $(".btn.btn-info.btn-lg").on('click',function() {
     data = {
@@ -26,7 +60,7 @@ $(".btn.btn-info.btn-lg").on('click',function() {
                 default:
                     $('#'+keys[i]).addClass('red').removeClass('green');
                     break;
-            } 
-        } 
+            }
+        }
     });
 });
